8b43a9ffbf9665bcca274a8251d08411f538498c
Cha Cha <midtic0404@gmail.com>
2023-09-12 16:11:34 -0600

    Setting up sequelize and Sqlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/user.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      storage: './db.sqlite3',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
